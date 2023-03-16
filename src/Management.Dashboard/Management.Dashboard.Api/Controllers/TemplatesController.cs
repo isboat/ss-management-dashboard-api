6ceb71ddeea63ae2287fb664e1a947d8923e4a89
isaac boateng <isboateng@hotmail.com>
2023-03-16 14:03:03 +0000

    add template work

---
+++ src/Management.Dashboard/Management.Dashboard.Api/Controllers/TemplatesController.cs
@@ -0,0 +1,27 @@
+﻿using Management.Dashboard.Models;
+using Management.Dashboard.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Management.Dashboard.Api.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class TemplatesController : ControllerBase
+    {
+        private readonly ITemplatesService _templatesService;
+
+        public TemplatesController(ITemplatesService templatesService)
+        {
+            _templatesService = templatesService;
+        }
+
+        [HttpGet("templates")]
+        [ProducesResponseType(200)]
+        public IEnumerable<KeyValuePair<string, IEnumerable<TemplatePropertyModel>>> Get()
+        {
+            return _templatesService.GetTemplates();
+        }
+    }
+}
